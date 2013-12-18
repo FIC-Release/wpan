6fff0590f2ecb83e46b6c5a88694a5488a435dfb
Vishal Mahaveer <vishalm@ti.com>
2013-12-18 16:33:56 -0600

    uim-sysfs: update sysfs entries of uim
    Update the sysfs entries according to latest kernel
    
    Change-Id: I39dd1d7945c50387e59e0db3c437afd258ca7eba
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- ti_st/uim-sysfs/uim.h
+++ ti_st/uim-sysfs/uim.h
@@ -134,10 +134,10 @@ typedef struct {
 	bdaddr_t addr;
 } __attribute__ ((packed)) uim_bdaddr_change_cmd;
 
-#define INSTALL_SYSFS_ENTRY	"/sys/devices/kim.21/install"
-#define DEV_NAME_SYSFS		"/sys/devices/kim.21/dev_name"
-#define BAUD_RATE_SYSFS		"/sys/devices/kim.21/baud_rate"
-#define FLOW_CTRL_SYSFS		"/sys/devices/kim.21/flow_cntrl"
+#define INSTALL_SYSFS_ENTRY	"/sys/devices/kim.22/install"
+#define DEV_NAME_SYSFS		"/sys/devices/kim.22/dev_name"
+#define BAUD_RATE_SYSFS		"/sys/devices/kim.22/baud_rate"
+#define FLOW_CTRL_SYSFS		"/sys/devices/kim.22/flow_cntrl"
 
 /* Functions to insert and remove the kernel modules from the system*/
 extern int init_module(void *, unsigned int, const char *);
